6045071abf5ddb655dc0e7247080ac2f0ccf757c
mohammad kamran <kamyzenon@gmail.com>
2022-08-27 12:25:04 +0430

    carousel lifecycle works currectly

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,25 @@
 import type { NextPage } from "next";
 import Carousel, { CarouselItem } from "../src/components/carousel/Carouse";
 
+// const carouselData = [{
+
+// }];
+
 const Home: NextPage = () => {
   return (
     <>
       <div className='flex justify-center w-full bg-gray-100 pt-8'>
         <Carousel>
           <CarouselItem>
-            <div className='bg-purple-500 h-full w-full inline-flex'></div>
+            <div className='bg-[url("../src/images/horizontal-banner-online-fashion-sale_23-2148585404.webp")] w-full h-full bg-contain bg-no-repeat'></div>
+            {/* <div className='w-full h-full bg-teal-500'></div> */}
           </CarouselItem>
           <CarouselItem>
-            <div className='bg-red-500 h-full w-full inline-flex'></div>
+            <div className='bg-[url("../src/images/elektronik-footer-banner.webp")] w-full h-full bg-contain bg-no-repeat'></div>
+            {/* <div className='w-full h-full bg-rose-500'></div> */}
           </CarouselItem>
           <CarouselItem>
-            <div className='bg-green-500 h-full w-full inline-flex'></div>
+            <div className='bg-blue-500 h-full w-full inline-flex'></div>
           </CarouselItem>
         </Carousel>
       </div>
--- src/components/carousel/Carouse.tsx
+++ src/components/carousel/Carouse.tsx
@@ -1,14 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 export const CarouselItem = (prop: any) => {
-  return prop.children;
+  return <div className='inline-flex justify-center items-center h-full w-full'>{prop.children}</div>;
 };
 
 const Carousel = (props: any) => {
   const [currentSlide, setCurrentSlide] = useState<number>(0);
 
+  const [stopCarousel, setStopCarousel] = useState<boolean>(false);
+
+  const updateSliede = (newSlideIndex: number) => {
+    if (newSlideIndex < 0) {
+      newSlideIndex = React.Children.count(props.children) - 1;
+    } else if (newSlideIndex >= React.Children.count(props.children)) {
+      newSlideIndex = 0;
+    }
+    setCurrentSlide(newSlideIndex);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!stopCarousel) {
+        updateSliede(currentSlide + 1);
+      }
+    }, 5000);
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+  }, [currentSlide, stopCarousel]);
+
   return (
-    <div className='w-11/12 h-[70vh] overflow-hidden relative '>
-      <div className='absolute left-0  z-10 w-24 h-full bg-stone-50 bg-opacity-0 flex justify-center items-center cursor-pointer transition-all duration-500  hover:bg-opacity-10'>
+    <div
+      className='w-11/12 h-[70vh] overflow-hidden relative'
+      onMouseEnter={() => setStopCarousel(true)}
+      onMouseLeave={() => setStopCarousel(false)}
+    >
+      <div
+        onClick={() => {
+          updateSliede(currentSlide - 1);
+        }}
+        className='absolute left-0  z-10 w-24 h-full bg-stone-50 bg-opacity-0 flex justify-center items-center cursor-pointer transition-all duration-500  hover:bg-opacity-10'
+      >
         <div className='bg-white  bg-opacity-75 shadow-2xl rounded-full w-9 h-9 inline-flex items-center justify-center text-slate-700'>
           <svg
             xmlns='http://www.w3.org/2000/svg'
@@ -22,7 +55,12 @@ const Carousel = (props: any) => {
           </svg>
         </div>
       </div>
-      <div className='absolute right-0 z-10 w-24 h-full bg-stone-50 bg-opacity-0 flex justify-center items-center cursor-pointer transition-all duration-500  hover:bg-opacity-10'>
+      <div
+        className='absolute right-0 z-10 w-24 h-full bg-stone-50 bg-opacity-0 flex justify-center items-center cursor-pointer transition-all duration-500  hover:bg-opacity-10'
+        onClick={() => {
+          updateSliede(currentSlide + 1);
+        }}
+      >
         <div className='bg-white  bg-opacity-75 shadow-2xl rounded-full w-9 h-9 inline-flex items-center justify-center text-slate-700'>
           <svg
             xmlns='http://www.w3.org/2000/svg'
@@ -36,21 +74,24 @@ const Carousel = (props: any) => {
           </svg>
         </div>
       </div>
-      <ul className='absolute bottom-0 w-full flex justify-center h-6'>
+      <ul className='inline-flex justify-center z-50 absolute bottom-0 w-full h-6'>
         {React.Children.map(props.children, (child: any, index: number) => {
           return (
             <li
               key={index}
               className='inline-flex items-center justify-center border border-slate-800 w-2 h-2 rounded-full mx-6 cursor-pointer'
             >
-              <button className={`${index === currentSlide && "bg-slate-800"} w-full h-full`} />
+              <button
+                onClick={() => setCurrentSlide(index)}
+                className={`${index === currentSlide && "bg-slate-800"} w-full h-full`}
+              />
             </li>
           );
         })}
       </ul>
       <div
         className={`whitespace-nowrap w-full h-full transition-all duration-500`}
-        style={{ transform: `translateX(-${2 * 100}%)` }}
+        style={{ transform: `translateX(-${currentSlide * 100}%)` }}
       >
         {React.Children.map(props.children, (child: any) => {
           return React.cloneElement(child);
