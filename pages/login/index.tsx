1865ee9781baa388d7c73eeb775423162dcecfe8
mohammadkamraan <kamyzenon@gmail.com>
2022-12-21 17:11:38 +0330

    Login page template completed (for light mode)

---
+++ pages/login/index.tsx
@@ -0,0 +1,74 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { ChangeEvent } from "react";
+
+const inputWrapperStyle = "flex flex-col relative mt-5";
+
+const inputStyle = "py-5 px-4 bg-transparent outline-none rounded-md border border-slate-200 shadow-md";
+
+const inputLabelStyle = "absolute -top-3 left-3 bg-transparent text-lg text-slate-800";
+
+const labelsSpan = "text-red-500 pb-2 mr-2";
+
+const Login: NextPage = () => {
+  const formChangeHandler = (event: ChangeEvent<HTMLFormElement>) => {
+    console.log("changing");
+  };
+
+  const formSubmitHandler = (event: ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("submits");
+  };
+
+  return (
+    <article className='max-h-screen h-screen flex items-center justify-center font-patrick'>
+      <Head>
+        <meta name='description' content='M Shop login page. Users can login to the website in this page.' />
+        <meta name='keywords' content='M shop login page,Login,login,singup,Singup' />
+        <meta name='author' content='Mohammad mahdi Kamran' />
+        <title>M Shop / Login</title>
+      </Head>
+      <section className='w-full md:w-[50%] max-w-[500px] border-2 bg-palette-card shadow-lg py-4 px-8 rounded-lg bg-neutral-50 mx-2 sm:mx-0'>
+        <header>
+          <h1 className='text-slate-700 text-3xl'>
+            <b>login</b>
+          </h1>
+          <h2 className='text-lg text-slate-800 pt-2'>Hi!</h2>
+          <p className='bg-neutral-100 py-1 px-2 text-[1rem] text-stone-500 my-1'>
+            Besides registering, you can also test the template with test account information(email: test@info.com -
+            Password: 123456)
+          </p>
+        </header>
+        <form onChange={formChangeHandler} onSubmit={formSubmitHandler}>
+          <div className={inputWrapperStyle}>
+            <label className={inputLabelStyle}>
+              <span className={labelsSpan}>*</span>
+              Email
+            </label>
+            <input placeholder='Please Enter Your Email' className={inputStyle} />
+          </div>
+          <div className={inputWrapperStyle}>
+            <label className={inputLabelStyle}>
+              <span className={labelsSpan}>*</span>
+              Password
+            </label>
+            <input placeholder='Please Enter Your Password' className={inputStyle} />
+          </div>
+          <button type='submit' className='w-full bg-rose-700 mt-5 py-4 rounded-md text-white text-2xl'>
+            Login
+          </button>
+        </form>
+        <div className='my-5'>
+          <Link href='/login'>
+            <a className='text-slate-700 text-lg'>
+              Don't You Have An Account? <span className='text-sky-400'>Sign Up</span>
+            </a>
+          </Link>
+        </div>
+      </section>
+    </article>
+  );
+};
+
+export default Login;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -246,26 +246,28 @@ const Navbar = () => {
             />
           </div>
         </div>
-        <button
-          className='w-32 md:w-auto h-9  border border-stone-200 rounded-md md:flex items-center text-slate-600 shadow-sm transition-all duration-300
-                hover:bg-slate-800 hover:text-stone-200 hidden'
-        >
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='h-6 w-6 ml-2'
-            fill='none'
-            viewBox='0 0 24 24'
-            stroke='currentColor'
-            strokeWidth={2}
+        <Link href='/login'>
+          <a
+            className='w-32 md:w-auto h-9  border border-stone-200 rounded-md md:flex items-center text-slate-600 shadow-sm transition-all duration-300
+                hover:bg-slate-800 hover:text-stone-200 dark:text-slate-300 hidden'
           >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1'
-            />
-          </svg>
-          <p className='ml-2 hidden md:inline px-2'>login | singup</p>
-        </button>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-6 w-6 ml-2'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1'
+              />
+            </svg>
+            <p className='ml-2 hidden md:inline px-2'>login | singup</p>
+          </a>
+        </Link>
         <button className='inline md:hidden ml-12 text-slate-800 dark:text-slate-300'>
           <svg
             xmlns='http://www.w3.org/2000/svg'
