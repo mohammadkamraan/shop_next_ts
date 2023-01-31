3045bc83cb511ac3305b1411dd2fbe3d6532a7a4
mohammadkamraan <kamyzenon@gmail.com>
2023-01-31 11:08:22 -0800

    GeneralProductCard component created

--- src/components/generalProductCard/GeneralProductCard.tsx
+++ src/components/generalProductCard/GeneralProductCard.tsx
@@ -1,7 +1,48 @@
 import { FC } from "react";
 
-const GeneralProductCard: FC = () => {
-  return <></>;
+import Link from "next/link";
+
+import { Product } from "../../typescript/INterfaces";
+
+import { categories } from "../../data/categoryOfGoodsData/categoryOfGoodsData";
+
+interface GeneralProductCardProps {
+  product: Product;
+}
+
+const GeneralProductCard: FC<GeneralProductCardProps> = ({ product }) => {
+  return (
+    <Link
+      href={`products/${categories[product.category]}/${product.id}`}
+      key={product.id}
+    >
+      <article className='relative h-[19rem] w-full rounded-md bg-sky-200 dark:bg-slate-700 dark:bg-opacity-80 my-auto overflow-hidden cursor-pointer'>
+        <figure className='h-48 w-full text-center p-2'>
+          <img
+            className='h-full w-full bg-none bg-sky-200 block'
+            src={product.image as string}
+            alt={product.title}
+          />
+        </figure>
+        <div>
+          <header>
+            <h6 className='text-center text-slate-800 dark:text-slate-300 text-lg'>
+              {product.title.length > 25
+                ? `${product.title.slice(0, 25)} ...`
+                : product.title}
+            </h6>
+          </header>
+          <p className='text-lg text-red-700 dark:text-pink-300 line-through ml-5'>
+            $ {product.price}
+          </p>
+          <p className='text-2xl text-slate-800 dark:text-slate-200 font-bold ml-5'>
+            ${(product.price - (product.price / 100) * 20).toFixed(2)}
+            <span className='text-sm text-teal-600'>(-20%)</span>
+          </p>
+        </div>
+      </article>
+    </Link>
+  );
 };
 
 export default GeneralProductCard;
--- src/components/specialOffers/productsSection/speciaOfferCard/SpecialOfferCard.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from "react";
-
-const SpecialOfferCard: FC = () => {
-  return <></>;
-};
-
-export default SpecialOfferCard;
