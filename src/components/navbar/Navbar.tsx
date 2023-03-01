37317d1acd0b070dcef14e627873bd7579c82d4e
mohammadkamraan <kamyzenon@gmail.com>
2023-03-01 17:48:07 +0330

    navbar linked to the cart page

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -385,25 +385,27 @@ const Navbar = () => {
           </Link>
         )}
         <div className='w-0.5 h-7 bg-neutral-300 rounded-lg ml-3 hidden md:inline' />
-        <span className='relative mr-5 ml-5'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='h-7 w-7 text-slate-700 ml-3 dark:text-slate-300'
-            fill='none'
-            viewBox='0 0 24 24'
-            stroke='currentColor'
-            strokeWidth={2}
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
-            />
-          </svg>
-          <span className='bg-rose-500 absolute -top-3 -right-2 w-6 h-6 rounded-full flex items-center justify-center text-neutral-50 text-xs'>
-            {status === "authenticated" ? cartData.totalAmount : 0}
-          </span>
-        </span>
+        <Link href='cart'>
+          <a className='relative mr-5 ml-5'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-7 w-7 text-slate-700 ml-3 dark:text-slate-300'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
+              />
+            </svg>
+            <span className='bg-rose-500 absolute -top-3 -right-2 w-6 h-6 rounded-full flex items-center justify-center text-neutral-50 text-xs'>
+              {status === "authenticated" ? cartData.totalAmount : 0}
+            </span>
+          </a>
+        </Link>
       </div>
       <Sidebar
         show={showSidebar}
