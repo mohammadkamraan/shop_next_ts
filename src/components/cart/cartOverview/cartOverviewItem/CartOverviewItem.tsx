09da9e879749f8889c3a845cda08f1a62f5a1c58
mohammadkamraan <kamyzenon@gmail.com>
2023-04-13 13:34:08 +0330

    file structure finished

--- src/components/cart/cartOverview/cartOverviewItem/CartOverviewItem.tsx
+++ src/components/cart/cartOverview/cartOverviewItem/CartOverviewItem.tsx
@@ -1,7 +1,28 @@
 import { FC } from "react";
 
-const CartOverviewItem: FC = () => {
-  return <></>;
+interface CartOverviewItemProps {
+  imageUrl: string;
+  description: string;
+  count: number;
+  price: number;
+  priceDiscount: number;
+}
+
+const CartOverviewItem: FC<CartOverviewItemProps> = ({
+  imageUrl,
+  description,
+  count,
+  price,
+  priceDiscount,
+}) => {
+  return (
+    <div>
+      <div className='flex items-center justify-around w-full'>
+        <img src={imageUrl} className='w-20 h-32' />
+        <p className='text-slate-700 dark:text-slate-300'>{description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default CartOverviewItem;
