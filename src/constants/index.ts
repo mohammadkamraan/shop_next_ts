67c7f0f04eed668736e66f3835b57a7502a82602
mohammadkamraan <kamyzenon@gmail.com>
2023-04-17 20:38:07 +0330

    domain constant fixed

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
 export const domain =
   process.env.NODE_ENV === "development"
     ? "localhost:3000"
-    : "https://shop-next-ts-mohammad-kamran.vercel.app/";
+    : "https://shop-next-ts-mohammad-kamran.vercel.app";
