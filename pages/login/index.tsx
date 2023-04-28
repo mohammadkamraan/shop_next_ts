3833dc91b06a899d5c1844cabc9b5845b604e5d3
mohammadkamraan <kamyzenon@gmail.com>
2023-04-28 21:07:37 +0330

    checked the route of nextauth :/

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,6 +1,5 @@
 import { NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 
